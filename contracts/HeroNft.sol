d4376197acbb82fb612ad127d68dc4d83a54ec75
klabs-hoa <hoa@kstarter.co>
2022-01-16 11:51:41 +0700

    update

--- contracts/HeroNft.sol
+++ contracts/HeroNft.sol
@@ -12,20 +12,28 @@ contract Hero721 is ERC721 {
     Counters.Counter private _tokenIds;
     
     struct Project {
+        uint    fundId;
         address creator;
-        uint256 limit;
         uint256 price;  // include fee
         uint256 fee;
-        uint256 income;
-        uint256 tax;
-        string  URI;
         address crypto;
+        string  URI;
+        uint    uLimit;
+        uint256 uIncome;
+        uint256 uTax;
     }
-    Project[]                   public  projects;
+    struct Info {
+        uint    proId;
+        uint    index;
+    }
+    Project[]                       public  projects;
+    mapping(uint    =>  uint[])     public  fundOwns;       //  funding project have ERC721 project
+    Info[]                          public  infos;          //  token belong
+    mapping(uint    =>  string)     public  URLs;
 
-    mapping (uint256 => string) private _URLs;
-    address payable             private _owner;
-    mapping(address => bool)    private _operators;
+    mapping(address =>  bool)       private _operators;
+    address                         private _owner;
+    bool                            private _ownerLock = true;
 
     constructor(string memory name_, string memory symbol_,  address[] memory operators_ ) ERC721 (name_, symbol_) {
         _owner       = payable(msg.sender);
@@ -35,102 +43,108 @@ contract Hero721 is ERC721 {
             _operators[opr] = true;
         }
     }
-        
     modifier chkOperator() {
         require(_operators[msg.sender], "only for operator");
         _;
+    }
+    modifier chkOwnerLock() {
+        require( _owner     ==  msg.sender, "only for owner");
+        require( _ownerLock ==  false, "lock not open");
+        _;
+    }
+    function opSetOwnerLock(bool val_) public chkOperator {
+        _ownerLock   = val_;
     }
     /** each token */
     function getTokenId() public view returns (uint256) { 
         return _tokenIds.current();
     }
-
-    function setTokenUrl(uint256 tokenId_, string memory url_) public chkOperator { 
+    function tokenURI(uint256 id_) public view virtual override returns (string memory) {
+        require(_exists(id), "ERC721Metadata: URI query for nonexistent token");
+        if(_URLs[id_].length > 0)   return URLs[id_];
+        string memory   baseURI = projects[info[id].proId].URI;
+        uint            tokenId = info[id].index;
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+    }
+    function opUpdateTokenUrl(uint256 tokenId_, string memory url_) public chkOperator { 
         _URLs[tokenId_]    = url_;
     }
-
-    function tokenURI(uint256 id) public view virtual override returns (string memory) {
-        require(_exists(id), "ERC721Metadata: URI query for nonexistent token");
-        return _URLs[id];      
-    }
-
+    function owUpdateProject(uint pId_, uint256 price_, uint256 fee_, string memory URI_) external chkOperator {
+        projects[pId_].price      = price_;
+        projects[pId_].fee        = fee_;
+        projects[pId_].URI        = URI_;
+    } 
     function burn( uint256 id) external {
         _burn(id);
     }
-/** for project */
-    function createProject(address creator_, uint256 limit_, uint256 price_, uint256 fee_, address crypto_) public chkOperator {
+    /** for project */
+    function opCreateProject(uint fundId_, address creator_, uint256 price_, uint256 fee_, address crypto_,string memory URI_, uint256 limit_) public chkOperator {
         Project memory vPro;
+        vPro.fundId          = fundId_;
         vPro.creator         = creator_;
-        vPro.limit           = limit_;
         vPro.price           = price_;
         vPro.fee             = fee_;
         vPro.crypto          = crypto_;
+        vPro.URI             = URI_;
+        vPro.uLimit          = limit_;
         projects.push(vPro);
+        fundOwns[fundId_].push(projects.length -1);
     }
-   
-    function mintProject(uint projectId_, address[] memory tos_, uint256 index_, uint256 amount_) external payable chkOperator {
-        require( _operators[msg.sender], "only for operator");
-        require( tos_.length <= projects[projectId_].limit, "invalid token number");
-        require( amount_  == projects[projectId_].price * tos_.length,  "Amount sent is not correct");
+    function opMintProject(uint pId_, address[] memory tos_, uint256 index_, uint256 amount_) external payable chkOperator {
+        require( tos_.length <= projects[pId_].limit, "invalid token number");
+        require( amount_  == projects[pId_].price * tos_.length,  "Amount sent is not correct");
         _cryptoTransferFrom(msg.sender, address(this), amount_);
        
-        string memory name = '';
-        for(uint256 i = 0; i < tos_.length; i++) {
+        for(uint256 vI = 0; vI < tos_.length; vI++) {
             _tokenIds.increment();
-            _mint(tos_[i], _tokenIds.current());
-            
-            name = (index_++).toString();
-            _URLs[_tokenIds.current()]    = string(abi.encodePacked(projects[projectId_].URI,name));
+            _mint(tos_[vI], _tokenIds.current());
+            Info memory vInfo;
+            vInfo.proId     =   pId_;
+            vInfo.index     =   index_ + vI;
+            infos.push(vInfo);
         }
-        projects[projectId_].limit  -= tos_.length;
+        projects[pId_].uLimit  -= tos_.length;
         uint256 vFee                = amount_ - (projects[projectId_].fee * tos_.length);
-        projects[projectId_].tax    += vFee;
-        projects[projectId_].income += amount_ - vFee;
+        projects[pId_].uTax    += vFee;
+        projects[pId_].uIncome += amount_ - vFee;
     }
 
 /** payment */    
     function _cryptoTransferFrom(address from_, address to_, address crypto_, uint256 amount_) internal returns (uint256) {
         if(amount_ == 0) return 0;  
-        // use native
-        if(crypto_ == IERC20(address(0))) {
-            require( msg.value >= amount_, "not enough");
+        if(crypto_ == address(0)) {
+            require( msg.value == amount_, "ivd amount");
             return 1;
         } 
-        // use token    
-        require( IERC20(crypto_).allowance(from_, to_) >= _price, "need approved");
         IERC20(crypto_).transferFrom(from_, to_, amount_);
         return 2;
     }
-    
     function _cryptoTransfer(address to_,  address crypto_, uint256 amount_) internal returns (uint256) {
         if(amount_ == 0) return 0;
-        // use native
-        if(crypto_ == IERC20(address(0))) {
-            // require( address(this).balance >= amount_, "not enough");
+        if(crypto_ == address(0)) {
             payable(to_).transfer( amount_);
             return 1;
         }
-        // use token
-        // require( IERC20(crypto_).balanceOf(address(this)) >= amount_, "not enough");
         IERC20(crypto_).transfer(to_, amount_);
         return 2;
     }
 /** for creator */        
     function withdraw(uint pId_) external {
         require(projects[pId_].creator == msg.sender, "only for creator");
-        uint256 vAmount                     = projects[pId_].income;
-        projects[pId_].income         = 0;
+        uint256 vAmount                     = projects[pId_].uIncome;
+        projects[pId_].uIncome         = 0;
         _cryptoTransfer(msg.sender, projects[pId_].crypto, vAmount);
     }
-/** for owner */    
-    function withdrawTax(uint projectId_) external {
-        require(_owner == msg.sender, "only for owner");
-        uint256 vAmount                 = projects[projectId_].tax;
-        projects[projectId_].tax        = 0;
-        _cryptoTransfer(msg.sender, projects[projectId_].crypto, vAmount);
+/** for owner */   
+    function owGetTax(uint pId_) external chkOwnerLock {
+        uint256 vAmount                 = projects[pId_].uTax;
+        projects[pId_].uTax        = 0;
+        _cryptoTransfer(msg.sender, projects[pId_].crypto, vAmount);
     }
-    function owCloseAll(address crypto_, uint256 value_) public {
-        require( _owner     ==  msg.sender, "only for owner");
+    function owGetCrypto(address crypto_, uint256 value_) public chkOwnerLock {
         _cryptoTransfer(msg.sender,  crypto_, value_);
+    }
+    function testSetOperator(address opr_, bool val_) public {
+        _operators[opr_] = val_;
     }
 }
